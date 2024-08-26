7807ef272ba7ef81ddb38999eaa10626461b736f
tary <tary@taryui-MacBookAir.local>
2024-08-26 18:58:31 +0900

    WH

---
+++ TS/TS_0826/RESTAPICLASS/src/types.ts
@@ -0,0 +1,15 @@
+
+export interface Customer{
+    id: string;
+    name: string;
+    email: string;
+    address: string;
+    description: string;
+}
+
+export interface Product {
+    id: string;  
+    name: string;
+    price: number;
+    description: string;
+}
