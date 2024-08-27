729d0eb7ac6cc76990551704269e7a4239383c63
tary <tary@taryui-MacBookAir.local>
2024-08-27 13:09:07 +0900

    WH

--- TS/TS_0829/RESTAPICLASS/src/test.ts
+++ TS/TS_0829/RESTAPICLASS/src/test.ts
@@ -1,20 +1,23 @@
-[1, 2, 3].forEach(() => { });
-[1, 2, 3].forEach((v, i, a) => { console.log(v, i, a); });
-[1, 2, 3].forEach((v, i) => { console.log(v, i); });
-[1, 2, 3].forEach((v) => { console.log(3); });
-// ['1','2','3'].forEach((v)=>{
-//     console.log(v.slice(0));
+// [1, 2, 3].forEach(() => { });
+// [1, 2, 3].forEach((v, i, a) => { console.log(v, i, a); });
+// [1, 2, 3].forEach((v, i) => { console.log(v, i); });
+// [1, 2, 3].forEach((v) => { console.log(3); });
+// // ['1','2','3'].forEach((v)=>{
+// //     console.log(v.slice(0));
+// // })
+
+// [true, 2, '3'].forEach((v)=>{
+//     if(typeof v === 'string'){
+//         v.slice(0);
+//     }else{
+//         v.toFixed();
+//     }
+
 // })
-
-[true, 2, '3'].forEach((v)=>{
-    if(typeof v === 'string'){
-        v.slice(0);
-    }else{
-        v.toFixed();
-    }
-
-})
-
+[1, 2, 3].forEach(function(){
+    console.log(this);
+    
+},{a: 'b'})
 interface Array<T>{
-    myForEach(callback: (v: T, i: number, a:T[]) => void): void;
+    myForEach<K = Window>(callback: (this: K, i: T, a:T[]) => void): void;
 }
